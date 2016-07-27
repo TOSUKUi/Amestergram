1a49b08b06d6359d2d7af2a363e5375370cb52b7
cubic <cubic@cbhd-pc-m013.local>
2016-07-27 15:47:32 +0900

    routing the signup action and others

--- amestergram/test/controllers/static_pages_controller_test.rb
+++ amestergram/test/controllers/static_pages_controller_test.rb
@@ -11,21 +11,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | #{title}"
   end
 
 end
