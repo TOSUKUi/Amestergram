afb5394d4e0edcc51bc69558e791e3f56ca5fd52
cubic <cubic@cbhd-pc-m013.local>
2016-07-27 15:20:07 +0900

    made stylesheet

--- amestergram/config/routes.rb
+++ amestergram/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
   root 'static_pages#home'
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get  'static_pages/contact'
+  get 'static_pages#about'
+  get 'static_pages#help'
+  get  'static_pages#contact'
+  get 'signup'  => 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
