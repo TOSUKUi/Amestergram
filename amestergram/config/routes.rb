db1e384e1833d16ee6f7f4083bb3f912e2404f47
TOSUKUi <am.businessmail.exp@gmail.com>
2016-08-03 18:25:19 +0900

    add the feature of signup and add footer

--- amestergram/config/routes.rb
+++ amestergram/config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
 
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- amestergram/test/controllers/users_controller_test.rb
+++ amestergram/test/controllers/users_controller_test.rb
@@ -6,4 +6,6 @@ class UsersControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+
+
 end
