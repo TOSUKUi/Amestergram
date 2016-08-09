f0e5ae4ada1968cd1a1a18a28337fd07bbe0c6b3
TOSUKUi <am.businessmail.exp@gmail.com>
2016-08-09 11:59:37 +0900

    add update function for users

--- amestergram/app/controllers/users_controller.rb
+++ amestergram/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
 
   def new
@@ -40,6 +41,12 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "you killed him."
+    redirect_to users_url
+  end
+
   private
 
     def user_params
@@ -57,6 +64,11 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless view_context.current_user?(@user)
+    end
+
+    #管理者かどうか確認
+    def admin_user
+     redirect_to(root_url) unless view_context.current_user.admin?
     end
 
 end
--- amestergram/db/seeds.rb
+++ amestergram/db/seeds.rb
@@ -5,10 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "Example User",
+User.create!(name:  "小路一平",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
